179710795960e0e362f2f6db83bd110c12888c75
Patrick <sleepingkirby@gmail.com>
2021-02-08 01:29:31 +0800

    modals being added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Button } from 'reactstrap';
+import { Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import SidePane from './sidePane';
 import Results from './results';
 import './App.css';
@@ -11,18 +11,25 @@ class App extends Component {
 	constructor(props){
 	super(props);
 		this.state={
-		signReg: '',
-			user: {
-			username: 'testUser',
-			token: 'bef5dace562f3f85ce59dedf8f481de0225ba07b89e8ed5bfd'
+		signReg: false,
+		user: {
+			username: '',
+			token: ''
 			},
-			search:{
+		search:{
 			fltrNm: '',
 			sort: ''
 			}
 		}
 
+	this.setModal=this.setModal.bind(this);
 	this.updtSrch=this.updtSrch.bind(this);
+	}
+
+	setModal(){
+		this.setState({
+		signReg: !this.state.signReg
+		});
 	}
 
 	//updates the search states
@@ -37,19 +44,31 @@ class App extends Component {
 		});
 	}
 
-
-
 	render() {
 
 		return (
 			<div className="App">
 				<div className="App-header">
 					<div className="label">Register</div>
-					<Button outline color="secondary">Log In</Button>
+					<Button outline color="secondary" onClick={this.setModal}>Log In</Button>
 				</div>
 				<div className="App-body">
           <SidePane inState={this.state.search} inUpdtSrch={this.updtSrch}/>
 					<Results inState={this.state} />
+
+					<div>
+						<Modal id="modLogin" isOpen={this.state.signReg} toggle={this.setModal} style={{'width':'300px'}}>
+							<ModalBody className="modBod">
+								<Input type="text" name="sortU" className="inpt" placeholder="Username" style={{'backgroundColor':'transparent', 'width':'250px', 'marginBottom':'10px'}}/>
+								<Input type="password" name="sortU" className="inpt" placeholder="Password" style={{'backgroundColor':'transparent', 'width':'250px' }} />
+							</ModalBody>
+							<ModalFooter>
+								<Button color="secondary" onClick={this.setModal}>Cancel</Button>
+								<Button color="primary" onClick={this.setModal}>Login</Button>{' '}
+							</ModalFooter>
+						</Modal>
+					</div>
+
 				</div>
 			</div>
 		);
